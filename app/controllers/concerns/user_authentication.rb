e2aa2999fd5f6cd24d12c5f7cf820dfb2b25db23
yamamoto kenta <yamamoto7.pgming@gmail.com>
2019-05-25 00:20:23 +0900

    user auth: create auth check method

--- app/controllers/concerns/user_authentication.rb
+++ app/controllers/concerns/user_authentication.rb
@@ -2,8 +2,43 @@ module UserAuthenticate
   extend ActiveSupport::Concern
 
   def set_user_info
+    if user_token_auth then
+      response.headers['sign_info'] = 'user'
+    else
+      response.headers['sign_info'] = 'none'
+    end
   end
 
   private
+  # get token from request header
+  def user_token_auth
+    token = request.headers['token']
+    if token
+      @current_user = authenticate_user(token)
+    else
+      return false
+    end
+  end
 
+  # check token
+  def authenticate_user token
+    unless token.include?(':u')
+      authenticate_error
+      return
+    end
+
+    user_id = token.split(':u').first
+    user = User.find_by(id: user_id)
+    if user && Devise.secure_compare(user.token, token)
+      return user
+    else
+      authenticate_error
+      return
+    end
+  end
+
+  # error: invalid token
+  def authenticate_error
+    render json: { error: 'devise.failure.unauthenticated' }, status: 401
+  end
 end
