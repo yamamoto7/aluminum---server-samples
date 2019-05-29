48f5f576738d5f8cce2038a241c6f48b96ee002b
yamamoto kenta <yamamoto7.pgming@gmail.com>
2019-05-29 14:38:05 +0900

    [fix: user] change token name to access_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # アクセストークンを更新
+  def update_access_token!
+    self.access_token = "#{self.id}:u#{Devise.friendly_token}"
+    save
+  end
 end
