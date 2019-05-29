94bb56be02d9dda1723ece2d5c57b325b83e96d0
yamamoto kenta <yamamoto7.pgming@gmail.com>
2019-05-29 13:12:25 +0900

    [fix: cording style] use brackets for args

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Users::RegistrationsController < ApplicationController
   def create
-    @user = User.new user_params
+    @user = User.new(user_params)
 
     if @user.save
       render json: @user
