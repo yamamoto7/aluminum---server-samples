1da58c064d9c50903284493d4b92e674d3541aee
yamamoto kenta <yamamoto7.pgming@gmail.com>
2019-05-29 12:42:01 +0900

    [add: users] session and registration's routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      # ユーザー関連
+      namespace :users do
+        resource :sign_in, only: [:create], controller: :sessions # users/sign_in
+        resource :sign_out, only: [:destroy], controller: :sessions #users/sign_out
+        resource :sign_up, only: [:create], controller: :registrations # users/sign_up
+      end
+    end
+  end
 end
