005ba8f954f5e02b502ab3b6b415798f8abccb89
yamamoto kenta <yamamoto7.pgming@gmail.com>
2019-05-29 12:36:03 +0900

    [create: users] resistration create simpliy

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::Users::RegistrationsController < ApplicationController
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      render json: @user
+    else
+      render json: { error: @user.errors }, status: :accepted
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password)
+  end
 end
