3820990bde44e93b66e5690bf0016a97a373ece0
yamamoto kenta <yamamoto7.pgming@gmail.com>
2019-06-03 10:45:15 +0900

    [create: auth] check auth from req header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,34 @@
 class ApplicationController < ActionController::API
+  # get token from request header
+  def user_token_auth
+    token = request.headers['token']
+    if token
+      @current_user = authenticate_user(token)
+    else
+      return false
+    end
+  end
+
+  private
+  # check token
+  def authenticate_user(token)
+    unless token.include?(':u')
+      authenticate_error
+      return
+    end
+
+    user_id = token.split(':u').first
+    user = Auth.find_by(id: user_id)
+    if user && Devise.secure_compare(user.token, token)
+      return user
+    else
+      authenticate_error
+      return
+    end
+  end
+
+  # error: invalid token
+  def authenticate_error
+    render json: { error: 'devise.failure.unauthenticated' }, status: :unauthorized
+  end
 end
